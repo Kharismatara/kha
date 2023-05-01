0eb338a4404d153db465fe0c23f2aa38803526c3
kharisma tara mukti <kharismataram@gmail.com>
2023-05-01 23:02:09 +0700

    menambahkan js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
-const url = "https://data.bps.go.id/api/v1/pusdatin/satuan/010100/indikator/93191/tahun/2018";
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    const tableData = document.getElementById("data-stunting");
-    data.forEach((item) => {
-      const row = tableData.insertRow();
-      const year = row.insertCell(0);
-      const prevalence = row.insertCell(1);
-      year.innerHTML = item.tahun;
-      prevalence.innerHTML = item.nilai;
-    });
-  });
+function cekStuntin(event) {
+  event.preventDefault();
+  //mendapatkan nilai input dari form
+  // Ambil input nama, umur, tinggi, dan berat badan
+  let nama = document.getElementById("nama").value;
+  let umur = parseInt(document.getElementById("umur").value);
+  let tinggi = parseFloat(document.getElementById("tinggi").value);
+  let berat = parseFloat(document.getElementById("bb").value);
+  // Ambil jenis kelamin yang dipilih
+  var jk = document.getElementById("jk").value;
+
+  //pengecekan bahwa nilaiinput 0 /minus maka alare
+  if (umur <= 0 || tinggi <= 0 || berat <= 0) {
+    alert("Masukan umur berat dan tinng tidak boleh minus");
+    return;
+    //kenapa  penggunakan tostring kkarena fungsi length hanya dapat digunakan pada objek bertipe string.
+    //Kode yang Anda berikan menggunakan fungsi toString() untuk mengkonversi angka menjadi string sehingga dapat memeriksa panjang digit dari angka tersebut.
+  } else if (berat.toString().length > 3 || tinggi.toString().length > 3) {
+    alert("Masukan umur, berat dan tinngi maksimal 3 digit angka");
+    return;
+  } else if (umur > 24) {
+    alert("masikmal penghitunagn 24 bulan");
+    return;
+  }
+  // pengecekan
+}
