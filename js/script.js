79669c1a23953a3fae79fc22a08f80b4cee95211
kharisma tara mukti <kharismataram@gmail.com>
2023-05-01 23:40:44 +0700

    penngecekan

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ function cekStuntin(event) {
   var jk = document.getElementById("jk").value;
 
   //pengecekan bahwa nilaiinput 0 /minus maka alare
-  if (umur <= 0 || tinggi <= 0 || berat <= 0) {
+  if (umur < 0 || tinggi <= 0 || berat <= 0) {
     alert("Masukan umur berat dan tinng tidak boleh minus");
     return;
     //kenapa  penggunakan tostring kkarena fungsi length hanya dapat digunakan pada objek bertipe string.
@@ -22,5 +22,91 @@ function cekStuntin(event) {
     alert("masikmal penghitunagn 24 bulan");
     return;
   }
-  // pengecekan
+  // pengecekan umur dan  tinggi dan jika laki -laki
+  let umurTinggi = "";
+  if ((umur = 0 && tinggi >= 46.1 && tinggi <= 55.6 && jk == "laki-laki")) {
+    umurTinggi = "Normal";
+    return;
+  } else if ((umur = 1 && tinggi >= 50.8 && tinggi <= 60.6 && jk == "laki-laki")) {
+    umurTinggi = "Normal";
+    return;
+  } else if ((umur = 2 && tinggi >= 54.4 && tinggi <= 60.6 && jk == "laki-laki")) {
+    umurTinggi = "Normal";
+    return;
+  } else if ((umur = 3 && tinggi >= 50.8 && tinggi <= 60.6 && jk == "laki-laki")) {
+    umurTinggi = "Normal";
+    return;
+  } else if ((umur = 4 && tinggi >= 50.8 && tinggi <= 60.6 && jk == "laki-laki")) {
+    umurTinggi = "Normal";
+    return;
+  } else if ((umur = 5 && tinggi >= 50.8 && tinggi <= 60.6 && jk == "laki-laki")) {
+    umurTinggi = "Normal";
+    return;
+  } else if ((umur = 1 && tinggi >= 50.8 && tinggi <= 60.6)) {
+    umurTinggi = "Normal";
+    return;
+  } else if ((umur = 6 && tinggi >= 50.8 && tinggi <= 60.6 && jk == "laki-laki")) {
+    umurTinggi = "Normal";
+    return;
+  } else if ((umur = 7 && tinggi >= 50.8 && tinggi <= 60.6 && jk == "laki-laki")) {
+    umurTinggi = "Normal";
+    return;
+  } else if ((umur = 8 && tinggi >= 50.8 && tinggi <= 60.6 && jk == "laki-laki")) {
+    umurTinggi = "Normal";
+    return;
+  } else if ((umur = 9 && tinggi >= 50.8 && tinggi <= 60.6 && jk == "laki-laki")) {
+    umurTinggi = "Normal";
+    return;
+  } else if ((umur = 10 && tinggi >= 50.8 && tinggi <= 60.6 && jk == "laki-laki")) {
+    umurTinggi = "Normal";
+    return;
+  } else if ((umur = 11 && tinggi >= 50.8 && tinggi <= 60.6 && jk == "laki-laki")) {
+    umurTinggi = "Normal";
+    return;
+  } else if ((umur = 12 && tinggi >= 50.8 && tinggi <= 60.6 && jk == "laki-laki")) {
+    umurTinggi = "Normal";
+    return;
+  } else if ((umur = 13 && tinggi >= 50.8 && tinggi <= 60.6 && jk == "laki-laki")) {
+    umurTinggi = "Normal";
+    return;
+  } else if ((umur = 14 && tinggi >= 50.8 && tinggi <= 60.6 && jk == "laki-laki")) {
+    umurTinggi = "Normal";
+    return;
+  } else if ((umur = 1 && tinggi >= 50.8 && tinggi <= 60.6 && jk == "laki-laki")) {
+    umurTinggi = "Normal";
+    return;
+  } else if ((umur = 15 && tinggi >= 50.8 && tinggi <= 60.6 && jk == "laki-laki")) {
+    umurTinggi = "Normal";
+    return;
+  } else if ((umur = 16 && tinggi >= 50.8 && tinggi <= 60.6 && jk == "laki-laki")) {
+    umurTinggi = "Normal";
+    return;
+  } else if ((umur = 17 && tinggi >= 50.8 && tinggi <= 60.6 && jk == "laki-laki")) {
+    umurTinggi = "Normal";
+    return;
+  } else if ((umur = 18 && tinggi >= 50.8 && tinggi <= 60.6 && jk == "laki-laki")) {
+    umurTinggi = "Normal";
+    return;
+  } else if ((umur = 19 && tinggi >= 50.8 && tinggi <= 60.6 && jk == "laki-laki")) {
+    umurTinggi = "Normal";
+    return;
+  } else if ((umur = 20 && tinggi >= 50.8 && tinggi <= 60.6 && jk == "laki-laki")) {
+    umurTinggi = "Normal";
+    return;
+  } else if ((umur = 21 && tinggi >= 50.8 && tinggi <= 60.6 && jk == "laki-laki")) {
+    umurTinggi = "Normal";
+    return;
+  } else if ((umur = 23 && tinggi >= 50.8 && tinggi <= 60.6 && jk == "laki-laki")) {
+    umurTinggi = "Normal";
+    return;
+  } else if ((umur = 23 && tinggi >= 50.8 && tinggi <= 60.6 && jk == "laki-laki")) {
+    umurTinggi = "Normal";
+    return;
+  } else if ((umur = 24 && tinggi >= 50.8 && tinggi <= 60.6 && jk == "laki-laki")) {
+    umurTinggi = "Normal";
+    return;
+  } else {
+    umurTinggi = "tidak normal";
+    return;
+  }
 }
